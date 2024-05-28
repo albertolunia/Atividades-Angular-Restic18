223ef83cfe7a3f8947abc3383444d4cf83cc7874
albertolunia <albertolunia@gmail.com>
2024-05-27 22:09:01 -0300

    Firebase

---
+++ .firebase/promanage-2ca1d/hosting/chunk-5RTIVNW5.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-POTE3WUC.js";import"./chunk-FBHGKCWK.js";import"./chunk-2RZM3PCF.js";export{a as ModuloDashboardModule};
--- P015/src/app/app.module.ts
+++ P015/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { CadastroComponent } from './modulo-cadastro/cadastro/cadastro.component
 import { LoginComponent } from './modulo-cadastro/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './modulo-cadastro/home/home.component';
-import { CadastroSuinoComponent } from './modulo-dashboard/cadastro-suino/cadastro-suino.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './modulo-dashboard/dashboard/dashboard.component';
 import { AutenticaInterceptor } from './service/autentica.interceptor';
 import { GraficoComponent } from './modulo-dashboard/grafico/grafico.component';
 import { MatIconModule } from '@angular/material/icon';
-import { HistoricoPesoComponent } from './modulo-dashboard/historico-peso/historico-peso.component';
-import { MinhaProducaoComponent } from './modulo-dashboard/minha-producao/minha-producao.component';
+
 import { HeaderComponent } from './modulo-dashboard/header/header.component';
 import { FooterComponent } from './modulo-dashboard/footer/footer.component';
 import { SessaoComponent } from './modulo-manejo/sessao/sessao.component';
